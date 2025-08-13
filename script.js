320bc961ac9e2691a1ddb5db5787ab209fca90d4
EnugurthiAnup <eanup.736@gmail.com>
2025-08-14 00:06:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ function mincost(arr)
 { 
 //write your code here
 	
-	arr.sort((a,b)=>{a-b})
+	arr.sort((a,b)=> a-b )
 	let sum=0
 	for (let i = 1; i < arr.length; i++) {
-		sum = arr[i] + arr[i-1]
-		arr.push(sum)
-		arr.sort((a,b)=>{a-b})
+		let cost = arr[i] + arr[i-1]
+		sum+=cost
+		arr.splice(0,2)
+		arr.push(cost)
+		arr.sort((a,b)=> a-b )
 		
 		
 	}
